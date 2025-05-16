21ec68092611327608d808c2019ab49a8e778f36
muthuabi <muthuabi292@gmail.com>
2025-05-16 12:34:58 +0530

    Compressed to ZIP

--- ecom/src/redux-app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import adminSlice from "./features/admin/adminSlice";
-
-export const store=configureStore({
-    reducer:{
-        admin:adminSlice
-    }
-});
